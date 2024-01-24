0144e390906e3e9079018a3eac55218279fd5bb4
AbraKadabraPuf <trai1s@yandex.ru>
2024-01-24 18:16:30 +0300

    capitalize.test.mjs

--- tests/capitalize.test.mjs
+++ tests/capitalize.test.mjs
@@ -1,8 +1,14 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
+import { strict as assert } from 'node:assert';
+
 import capitalize from '../src/capitalize.mjs';
 
+assert(capitalize('hello') === 'Hello');
+assert(capitalize('') === '');
+
+/*
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
 }
@@ -10,5 +16,5 @@ if (capitalize('hello') !== 'Hello') {
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
 }
-
+*/
 console.log('Все тесты пройдены!');
